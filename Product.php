6244389bb885fab34b1706565386c4a08f24be1b
iriska74 <iskairwan03012@gmail.com>
2025-08-18 11:42:40 +0700

    Add files via upload

--- Product.php
+++ Product.php
@@ -2,9 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    use HasFactory;
+
+    // Kolom yang boleh diisi massal (fillable)
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'image_url',
+        'category_id',
+    ];
+
+    /**
+     * Relasi ke Category
+     * Satu produk punya satu kategori
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
